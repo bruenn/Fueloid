4b06cbd9230a97e7edac285a057b72f6fd9169a9
gpb <mrbruenn@gmail.com>
2011-03-11 01:42:29 +0100

    refactor database abstraction rename statistic -> vehicle

---
+++ src/biz/bruenn/fueloid/data/FueloidDatabaseHelper.java
@@ -0,0 +1,72 @@
+package biz.bruenn.fueloid.data;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+/**
+ * This class helps open, create, and upgrade the database file.
+ */
+public class FueloidDatabaseHelper extends SQLiteOpenHelper {
+	public Context mContext;
+	
+    static final String TAG = "FueloidDatabaseHelper";
+    static final String DATABASE_NAME = "fueloid.db";
+    static final int DATABASE_VERSION = 18;
+	
+    public FueloidDatabaseHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+        mContext = context;
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+    	db.execSQL(FillUp.SQL_CREATE_TABLE);
+    	db.execSQL(Vehicle.SQL_CREATE_TABLE);
+    	db.execSQL(VehicleFillupColumns.SQL_CREATE_TABLE);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        Log.w(TAG, "Upgrading database from version " + oldVersion + " to "
+                + newVersion + ", which will destroy all old data");
+        db.execSQL("DROP TABLE IF EXISTS " + FillUp.TABLE_NAME);
+        db.execSQL("DROP TABLE IF EXISTS " + Vehicle.TABLE_NAME);
+        db.execSQL("DROP TABLE IF EXISTS " + VehicleFillupColumns.TABLE_NAME);
+        onCreate(db);
+    }
+    
+    /**
+     * Wrapper to SQLiteDatabase.rawQuery()
+     * If an exception occurs it would be catch inside and null will be returned
+     * @param sql the SQL query. The SQL string must not be ; terminated
+     * @param selectionArgs You may include ?s in where clause in the query,
+     *        which will be replaced by the values from selectionArgs. The values
+     *        will be bound as Strings.
+     * @return null or a Cursor object, which is positioned at the first entry.
+     * 		   Note that Cursors are not synchronized, see the documentation for
+     * 		   more details.
+     */
+    public Cursor protectedRawQuery(String sql, String[] selectionArgs) {
+		SQLiteDatabase db = null;
+		Cursor result = null;
+		try {
+			db = getReadableDatabase();
+			if(null != db) {
+				result = db.rawQuery(sql, selectionArgs);
+				if(null != result) {
+					result.moveToFirst();
+				}
+			}
+			return result;
+	    } catch (Exception e) {
+	    	return null;    		
+	    } finally {
+	    	if(null != db ) {
+	    		db.close();
+	    	}
+	    }
+	}
+}
---
+++ src/biz/bruenn/fueloid/data/VehicleFillupColumns.java
@@ -0,0 +1,109 @@
+/**
+		Copyright (C) 2011 Patrick Brünn.
+
+    This file is part of Fueloid.
+
+    Fueloid is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Fueloid is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Fueloid.  If not, see <http://www.gnu.org/licenses/>. */
+
+package biz.bruenn.fueloid.data;
+
+
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.provider.BaseColumns;
+
+public class VehicleFillupColumns implements BaseColumns {
+	public static final String TABLE_NAME = "ltvf";//"linktable_vehicle_fillups";
+	public static final String VID = "sid"; //vehicle id
+	public static final String FID = "fid"; //fill-up id	
+	public static final String COLSID = TABLE_NAME + "." + VID;
+	public static final String COLFID = TABLE_NAME + "." + FID;
+	
+	public static final String FILLUPS_OF_VEHICLE = FillUp.TABLE_NAME + ", " + TABLE_NAME + " " +
+		"WHERE " + COLFID + "=" + FillUp.COLID + " AND " + COLSID + "= ?;";
+	
+	public static final String SQL_CREATE_TABLE =
+		"CREATE TABLE " + TABLE_NAME + " ("
+			+ _ID +" INTEGER PRIMARY KEY AUTOINCREMENT, "
+			+ VID +" INTEGER, "
+			+ FID +" INTEGER);";
+
+	public static boolean delete(FueloidDatabaseHelper dh, Vehicle s, FillUp f) {
+		SQLiteDatabase db = null;
+		try {
+			db = dh.getWritableDatabase();
+			if(null != db) {
+				db.delete(TABLE_NAME, VID + "=" + s.getmId() + " AND " + FID + "=" + f.getmId(), null);
+			}
+			return false;
+		} catch (Exception e) {
+			return false;
+		} finally {
+    		if(null != db ) {
+    			db.close();
+    		}
+    	}
+	}
+	
+	public static Cursor getFillUpsOfVehicle(FueloidDatabaseHelper dh, Vehicle s) {
+		SQLiteDatabase db = null;
+		try {
+			db = dh.getReadableDatabase();
+			if(null != db) {
+				Cursor c = db.rawQuery(					
+					"SELECT " + FillUp.COLID + ", " +
+								FillUp.COLDISTANCE + ", " +
+								FillUp.FILLDATE + ", " +
+								FillUp.COLLITER + ", " +
+								FillUp.COLMONEY + 
+					" FROM " + FillUp.TABLE_NAME + ", " + TABLE_NAME +
+					" WHERE " + COLFID + "=" + FillUp.COLID + " AND " + COLSID + "=?"+
+					" ORDER BY " + FillUp.COLFILLDATE + " DESC", new String[] {String.valueOf(s.getmId())});
+				if(null != c) {
+					c.moveToFirst();
+				}
+				return c;
+			}
+			return null;
+    	} catch (Exception e) {
+    		return null;    		
+    	} finally {
+    		if(null != db ) {
+    			db.close();
+    		}
+    	}
+	}
+	
+	public static final boolean insert(FueloidDatabaseHelper dh, Vehicle s, FillUp f) {
+		SQLiteDatabase db = null;
+		try {
+			db = dh.getWritableDatabase();
+			if(null != db) {
+				ContentValues values = new ContentValues();
+				values.put(VID, s.getmId());
+				values.put(FID, f.getmId());
+				db.insert(TABLE_NAME, null, values);
+				return true;
+			}
+			return false;
+		} catch (Exception e) {
+			return false;
+		} finally {
+			if(null != db ) {
+    			db.close();
+			}
+    	}
+	}
+}
